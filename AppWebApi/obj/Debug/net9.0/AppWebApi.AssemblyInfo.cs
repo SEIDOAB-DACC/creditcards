63bed8fce6735c34ec3febc86e9c0ba046324ecf
Martin <martin_lenart@icloud.com>
2025-09-01 11:37:47 +0200

    template done

--- AppWebApi/obj/Debug/net9.0/AppWebApi.AssemblyInfo.cs
+++ AppWebApi/obj/Debug/net9.0/AppWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bdc3c09e3daa780e4eca7044c00d4cc2a49a1e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ readme-clr1.txt
@@ -0,0 +1,10 @@
+To create the AppWebApi
+
+1. Run AppWebApi with or without debugger
+
+   Without debugger:   
+   Open a Terminal in folder AppWebApi run: 
+   dotnet run -lp https 
+   open url: https://localhost:7066/weatherforecast
+
+2. You will see the result from the endpoit weatherforecast
