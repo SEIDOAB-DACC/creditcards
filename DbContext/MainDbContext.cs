01476aeb26d70dc92b4b0e128cd7e2f83018e15c
Martin <martin_lenart@icloud.com>
2025-09-01 12:47:02 +0200

    DbContext and migration done

---
+++ DbContext/MainDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+using Configuration;
+using DbModels;
+using Microsoft.Extensions.Hosting.Internal;
+
+namespace DbContext;
+
+//DbContext namespace is a fundamental EFC layer of the database context and is
+//used for all Database connection as well as for EFC CodeFirst migration and database updates 
+public class MainDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbSet<CreditCardDbM> CreditCards { get; set; }
+
+    public MainDbContext() { }
+
+    public MainDbContext(DbContextOptions<MainDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Design time: manually create configuration to read appsettings.json
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var config = configBuilder.Build();
+
+            var connectionString = config.GetConnectionString("SqlServerDocker");
+            optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure());
+        }
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<decimal>().HaveColumnType("money");
+        configurationBuilder.Properties<string>().HaveColumnType("varchar(200)");
+
+        base.ConfigureConventions(configurationBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
